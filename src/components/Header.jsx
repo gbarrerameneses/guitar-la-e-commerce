77ffa01e49528c92643b8a906b676edbde483384
Gustavo Barrera <gustavo.8805@gmail.com>
2024-03-27 10:13:59 -0600

    corrigiendo state derivado isEmpty() y calculando el total a pagar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ const Header = ({cart}) => {
 
     // State Derivado
     const isEmpty = () => cart.length === 0
+    const cartTotal = () => cart.reduce( (total, item) => total + (item.quantity * item.price), 0)
 
     return (
     <>
@@ -21,10 +22,11 @@ const Header = ({cart}) => {
 
                         <div id="carrito" className="bg-white p-3">
 
-                            {isEmpty ? (
+                            {isEmpty() ? (
                                 <p className="text-center">El carrito esta vacio</p>
                             ) : (
-                            <table className="w-100 table">
+                           <>
+                             <table className="w-100 table">
                                 <thead>
                                     <tr>
                                         <th>Imagen</th>
@@ -71,8 +73,9 @@ const Header = ({cart}) => {
                                     ))}
                                 </tbody>
                             </table>
+                            <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                            </>
                             )}
-                            <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                         </div>
                     </div>
